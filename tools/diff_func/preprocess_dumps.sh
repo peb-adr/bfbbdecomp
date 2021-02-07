6b70b887cbbce52fd03f1b502962e2541677d8a2
Adrian Richter <adrichter97@gmail.com>
2021-02-08 00:29:16 +0100

    add stop address feature

--- tools/diff_func/preprocess_dumps.sh
+++ tools/diff_func/preprocess_dumps.sh
@@ -2,10 +2,12 @@
 
 tool_directory=tools/diff_func
 
-address_start=$(<$tool_directory/start_address)
-address_stop=$(<$tool_directory/stop_address)
+address_start=$(echo $(<$tool_directory/address_start) | tr '[:upper:]' '[:lower:]')
+address_stop=$(echo $(<$tool_directory/address_stop) | tr '[:upper:]' '[:lower:]')
+
 regex_start='^'$address_start':.*'
-regex_stop='^[A-Fa-f0-9]{8}:\s*[A-Fa-f0-9]{2}\s*[A-Fa-f0-9]{2}\s*[A-Fa-f0-9]{2}\s*[A-Fa-f0-9]{2}\s*blr.*'
+regex_stop='^'$address_stop':.*'
+regex_blr='^[A-Fa-f0-9]{8}:\s*[A-Fa-f0-9]{2}\s*[A-Fa-f0-9]{2}\s*[A-Fa-f0-9]{2}\s*[A-Fa-f0-9]{2}\s*blr.*'
 
 if [ -z "$address_start" ]
 then
@@ -13,7 +15,13 @@ then
     stop_cmd=''
 else
     start_cmd='-start '$regex_start
-    stop_cmd='-stop '$regex_stop
+
+    if [ -z "$address_stop" ]
+    then
+        stop_cmd='-stop '$regex_blr
+    else
+        stop_cmd='-stop '$regex_stop
+    fi
 fi
 
 $tool_directory/dif/dif baserom.dump $start_cmd $stop_cmd -gui '' -stdout > $tool_directory/baserom_pp.dump
